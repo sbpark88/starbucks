01acdba85114c56a557bc1c67cb2a05f37d50627
sbpark88 <saebyulpark88@gmail.com>
2024-02-25 23:19:17 +0900

    Close #20: animation with intersection observer

--- js/components/main/animation-effect/middle/PickYourFavorite.js
+++ js/components/main/animation-effect/middle/PickYourFavorite.js
@@ -6,9 +6,17 @@ const container = $(".animation-effect .pick-your-favorite");
 const template = `
 <div class="inner">
   <div class="text-group">
-    <img src="../../../../../images/favorite_text1.png" alt="" class="title" />
-    <img src="../../../../../images/favorite_text2.png" alt="" class="description" />
-    <div class="more">
+    <img
+      src="../../../../../images/favorite_text1.png"
+      alt=""
+      class="title back-to-position to-right"
+    />
+    <img
+      src="../../../../../images/favorite_text2.png"
+      alt=""
+      class="description back-to-position to-right delay-300"
+    />
+    <div class="more back-to-position to-right delay-600">
       <a href="javascript:void(0)" class="btn btn--white">자세히 보기</a>
     </div>
   </div>
--- js/components/main/animation-effect/top/ReserveCoffee.js
+++ js/components/main/animation-effect/top/ReserveCoffee.js
@@ -5,14 +5,26 @@ import { $, render } from "../../../../utils/render.js";
 const container = $(".animation-effect .reserve-coffee");
 const template = `
 <div class="inner">
-  <img src="../../../../../images/reserve_logo.png" alt="" class="reserve-logo" />
+  <img
+    src="../../../../../images/reserve_logo.png"
+    alt=""
+    class="reserve-logo back-to-position to-right"
+  />
   <div class="text-group">
-    <img src="../../../../../images/reserve_text.png" alt="" class="description" />
-    <div class="more">
+    <img
+      src="../../../../../images/reserve_text.png"
+      alt=""
+      class="description back-to-position to-right delay-300"
+    />
+    <div class="more back-to-position to-right delay-600">
       <a href="javascript:void(0)" class="btn btn--gold">자세히 보기</a>
     </div>
   </div>
-  <img src="../../../../../images/reserve_image.png" alt="" class="product" />
+  <img
+    src="../../../../../images/reserve_image.png"
+    alt=""
+    class="product back-to-position to-left delay-900"
+  />
 </div>
 `;
 
