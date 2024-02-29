9e233a8dc581825d9340e6c6a1f1b492f36475b7
sbpark88 <saebyulpark88@gmail.com>
2024-02-29 22:06:43 +0900

    Close #23: Add sign-in page

--- src/js/apps/SignIn.js
+++ src/js/apps/SignIn.js
@@ -1,6 +1,8 @@
 // @ts-check
 
+import "../../css/signIn/index.scss";
+
 import { init } from "../utils/render.js";
 import template from "./SignIn.html";
 
-init(template);
+init(template)(() => {});
